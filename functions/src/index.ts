e679d9e24f430edb00b1702dec297872dda63559
Brata Saputra <my.developer@outlook.com>
2021-09-14 08:12:07 +0700

    Use forward to header value to create proxy

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,14 +7,6 @@ const redditAPI = express();
 const redditAPIRouterV1 = express.Router();
 
 const nonauthHostname = "https://www.reddit.com";
-const stylesRedditMediaHostname = "https://styles.redditmedia.com";
-const athumbsRedditMediaHostname = "https://a.thumbs.redditmedia.com";
-const bthumbsRedditMediaHostname = "https://b.thumbs.redditmedia.com";
-const cthumbsRedditMediaHostname = "https://c.thumbs.redditmedia.com";
-const dthumbsRedditMediaHostname = "https://d.thumbs.redditmedia.com";
-const ethumbsRedditMediaHostname = "https://e.thumbs.redditmedia.com";
-const fthumbsRedditMediaHostname = "https://f.thumbs.redditmedia.com";
-const gthumbsRedditMediaHostname = "https://g.thumbs.redditmedia.com";
 
 redditAPI.use("/v1", redditAPIRouterV1);
 
@@ -30,14 +22,10 @@ const generateProxy = (hostname: string, pathToRewrite: string) => {
   return createProxyMiddleware(proxyOption);
 };
 
-redditAPIRouterV1.use("/api/*", generateProxy(nonauthHostname, "^/v1/api"));
-redditAPIRouterV1.use("/styles/*", generateProxy(stylesRedditMediaHostname, "^/v1/styles"));
-redditAPIRouterV1.use("/athumbs/*", generateProxy(athumbsRedditMediaHostname, "^/v1/athumbs"));
-redditAPIRouterV1.use("/bthumbs/*", generateProxy(bthumbsRedditMediaHostname, "^/v1/bthumbs"));
-redditAPIRouterV1.use("/cthumbs/*", generateProxy(cthumbsRedditMediaHostname, "^/v1/cthumbs"));
-redditAPIRouterV1.use("/dthumbs/*", generateProxy(dthumbsRedditMediaHostname, "^/v1/dthumbs"));
-redditAPIRouterV1.use("/ethumbs/*", generateProxy(ethumbsRedditMediaHostname, "^/v1/ethumbs"));
-redditAPIRouterV1.use("/fthumbs/*", generateProxy(fthumbsRedditMediaHostname, "^/v1/fthumbs"));
-redditAPIRouterV1.use("/gthumbs/*", generateProxy(gthumbsRedditMediaHostname, "^/v1/gthumbs"));
+redditAPIRouterV1.use("/proxy/*", (req, res, next) => {
+  const forwardedHostName = req.header("Brata-Forward-To") || nonauthHostname;
+  const proxyMiddleware = generateProxy(forwardedHostName, "^.*/proxy");
+  return proxyMiddleware(req, res, next);
+});
 
 export const reddit = functions.region("asia-southeast2").https.onRequest(redditAPI);
